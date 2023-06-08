6eafc637a6dae42dfdf176e98ec20ada96dc5bae
Colin Leary <113806491+IncumbentPROGRESSIVE@users.noreply.github.com>
2023-06-08 17:19:41 -0400

    Update index.js
    update/create

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+ var userInput = document.getElementById("userInput");
+    var messageBox = document.getElementById("message");
+    var messageBox2 = document.getElementById("message2");
+    var month = document.getElementById("month");
+    var day = document.getElementById("day");
+    var year = document.getElementById("year");
 
+    function newMessage() {
+      userInput.addEventListener("input", function() {
+        var message = userInput.value + " " + month.value + "/" + day.value + "/" + year.value;
+        messageBox.value = message;
+        messageBox2.value = message;
+      });
+    }
+
+    newMessage();
